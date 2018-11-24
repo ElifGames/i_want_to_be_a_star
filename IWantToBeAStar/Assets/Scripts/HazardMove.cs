96d5377226c052316dab89e2f8fd922ffdae7e93
pid011 <deet1022@gmail.com>
2018-11-24 22:41:08 +0900

    플레이어 움직임 구현, 위험요소와의 상호작용 구현

---
+++ IWantToBeAStar/Assets/Scripts/HazardMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardMove : MonoBehaviour 
+{
+    public float speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = transform.up * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
