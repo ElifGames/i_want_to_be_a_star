615fc6f8e84ca160d576a78a8619e7a771dbfec3
pid011 <deet1022@gmail.com>
2018-11-25 01:03:10 +0900

    게임컨트롤 코드 생성

--- IWantToBeAStar/Assets/Scripts/GameController.cs
+++ IWantToBeAStar/Assets/Scripts/GameController.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector2 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine(SpawnWaves());
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
 	}
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector2 spawnPosition = new Vector2(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
 }
