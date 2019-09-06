3da2b2202f19df9d94ac25cf5c42335829903c14
Sepi <deet1022@gmail.com>
2019-09-06 15:54:57 +0900

    점수를 플레이어 머리 위에 위치하도록 변경

---
+++ IWantToBeAStar/Assets/Scripts/MainGame/MapObjects/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace IWantToBeAStar.MainGame.MapObjects
+{
+    public class FollowPlayer : MonoBehaviour
+    {
+        public float borderX;
+        public float borderY;
+
+        private GameObject player;
+
+        private void Awake()
+        {
+            player = GameObject.Find("Player");
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = player.transform.position + new Vector3(borderX, borderY);
+        }
+    }
+}
