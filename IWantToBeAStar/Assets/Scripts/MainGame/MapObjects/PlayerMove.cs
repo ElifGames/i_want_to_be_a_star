fea417613ea049ddbda4286d4c2ad3596f3c7f38
Sepi <deet1022@gmail.com>
2019-09-06 16:24:24 +0900

    마우스 움직임이 빠릿빠릿 하도록 수정

--- IWantToBeAStar/Assets/Scripts/MainGame/MapObjects/PlayerMove.cs
+++ IWantToBeAStar/Assets/Scripts/MainGame/MapObjects/PlayerMove.cs
@@ -44,8 +44,10 @@ namespace IWantToBeAStar.MainGame.MapObjects
 
         private void MouseControl()
         {
-            transform.position = Vector2.Lerp
-                                (transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), CursorSpeed);
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //부드럽게 움직이는 코드
+            //transform.position = 
+            //    Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), CursorSpeed);
 
             // 플레이어가 맵 밖을 나가지 않도록 하는 코드
             transform.position = new Vector2(
