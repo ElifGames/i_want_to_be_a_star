8b7b8daaa96390ac2d5dcf5862cf02d3b0d67b7f
Sepi <deet1022@gmail.com>
2019-09-09 05:46:15 +0900

    번개 생성 알고리즘 개선

--- IWantToBeAStar/Assets/Scripts/MainGame/HazardManager.cs
+++ IWantToBeAStar/Assets/Scripts/MainGame/HazardManager.cs
@@ -1,5 +1,8 @@
 ﻿using IWantToBeAStar.MainGame.MapObjects.Hazards;
 using System.Collections;
+using System.Linq;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -52,11 +55,18 @@ namespace IWantToBeAStar.MainGame
             GameData.RightPosition = RightPosition;
         }
 
+        /// <summary>
+        /// 게임 시작 시 최초 장애물을 생성합니다.
+        /// </summary>
         private void HandleGameStartEvent()
         {
             StartCoroutine("StartSpawningLeftRightMove", Bird);
         }
 
+        /// <summary>
+        /// <paramref name="changedStage"/>에 맞게 장애물을 생성합니다.
+        /// </summary>
+        /// <param name="changedStage">바뀌는 스테이지</param>
         private void HandleStageChangedEvent(Stage changedStage)
         {
             StopAllCoroutines();
@@ -102,6 +112,10 @@ namespace IWantToBeAStar.MainGame
             }
         }
 
+        /// <summary>
+        /// 왼쪽 과 오른족 중 랜덤으로 하나를 골라 스폰합니다.
+        /// </summary>
+        /// <param name="hazard"></param>
         private void SpawnLeftOrRight(GameObject hazard)
         {
             int i = Random.Range(0, 100);
@@ -115,19 +129,25 @@ namespace IWantToBeAStar.MainGame
             }
         }
 
+        /// <summary>
+        /// 번개 생성을 무한반복합니다.
+        /// </summary>
+        /// <returns></returns>
         private IEnumerator StartSpawningLightning()
         {
             while (true)
             {
-                int count = Random.Range(0, 3);
-                for (int i = 0; i <= count; i++)
-                {
-                    SpawnHazard(Lightning, Direction.Center);
-                }
-                yield return new WaitForSeconds(GameData.SpawnWait + 0.5f);
+                int count = Random.Range(0, 4);
+                SpawnHazard(Lightning, Direction.Center, count);
+
+                yield return new WaitForSeconds(GameData.SpawnWait + 0.2f);
             }
         }
 
+        /// <summary>
+        /// 우주 장애물을 현재 상태에 맞게 생성하는 것을 무한반복합니다.
+        /// </summary>
+        /// <returns></returns>
         private IEnumerator StartSpawningSpaceHazards()
         {
             while (true)
@@ -150,62 +170,91 @@ namespace IWantToBeAStar.MainGame
             }
         }
 
-        private void SpawnHazard(GameObject hazard, Direction direction)
+        /// <summary>
+        /// 위, 아래, 왼쪽, 오른쪽 중 한 구간을 골라 랜덤한 위치에 생성합니다.
+        /// </summary>
+        /// <param name="hazard">생성할 장애물</param>
+        /// <param name="direction">방향</param>
+        /// <param name="spawnCount">
+        /// <paramref name="spawnCount"/>가 2개 이상일 경우 서로 겹치지 않게 생성합니다.
+        /// </param>
+        private void SpawnHazard(GameObject hazard, Direction direction, int spawnCount = 1)
         {
-            Vector2 spawnPosition = new Vector2();
-
-            switch (direction)
+            // Array를 쓸 경우 값을 미리 초기화해야되기 때문에 초기화된 값때문에 [0, 0, 0]이 먼저 입력되어버림
+            List<Vector2> spawnedPositions = new List<Vector2>();
+            for (int i = 0; i < spawnCount; i++)
             {
-                case Direction.Up:
-                    spawnPosition = new Vector2
-                        (Random.Range(-UpPosition.x, UpPosition.x), UpPosition.y);
-                    break;
+                float posX = 0f;
+                float posY = 0f;
+                switch (direction)
+                {
+                    case Direction.Up:
+                        posX = GetNotOverlappedRandomPosition(spawnedPositions, true, UpPosition.x);
+                        posY = UpPosition.y;
+                        break;
 
-                case Direction.Left:
-                    spawnPosition = new Vector2
-                        (LeftPosition.x, Random.Range(-LeftPosition.y, LeftPosition.y));
-                    break;
+                    case Direction.Left:
+                        posX = LeftPosition.x;
+                        posY = GetNotOverlappedRandomPosition(spawnedPositions, false, LeftPosition.y);
+                        break;
 
-                case Direction.Right:
-                    spawnPosition = new Vector2
-                        (RightPosition.x, Random.Range(-RightPosition.y, RightPosition.y));
-                    break;
+                    case Direction.Right:
+                        posX = RightPosition.x;
+                        posY = GetNotOverlappedRandomPosition(spawnedPositions, false, RightPosition.y);
+                        break;
 
-                case Direction.Center:
-                    spawnPosition = new Vector2
-                        (Random.Range(-UpPosition.x, UpPosition.x), 0);
-                    break;
+                    case Direction.Center:
+                        posX = GetNotOverlappedRandomPosition(spawnedPositions, true, UpPosition.x);
+                        posY = 0f;
+                        break;
+                }
+                var spawnPosition = new Vector2(posX, posY);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                spawnedPositions.Add(spawnPosition);
             }
-
-            Quaternion spawnRotation = Quaternion.identity;
-            Instantiate(hazard, spawnPosition, spawnRotation);
         }
 
-        //private IEnumerator RemoveNotDelectedLightnings()
-        //{
-        //    for (int i = 0; i < 10; i++)
-        //    {
-        //        var list = GameObject.FindGameObjectsWithTag("Notification");
-        //        var list2 = GameObject.FindGameObjectsWithTag("Hazard");
-        //        if (list.Length != 0)
-        //        {
-        //            foreach (var item in list)
-        //            {
-        //                Destroy(item);
-        //            }
-        //        }
-        //        if (list2.Length != 0)
-        //        {
-        //            foreach (var item in list2)
-        //            {
-        //                if (item.name == "Lightning")
-        //                {
-        //                    Destroy(item);
-        //                }
-        //            }
-        //        }
-        //        yield return new WaitForSeconds(0.1f);
-        //    }
-        //}
+        /// <summary>
+        /// <paramref name="positions"/>에서 +1 -1 위치 까지 겹치지 않는 랜덤 숫자를 뽑아서 반환합니다.
+        /// </summary>
+        /// <param name="positions">대조해볼 값들</param>
+        /// <param name="findX">x 위치인지 y위치인지를 알려줌</param>
+        /// <param name="MaxRandomNumber">최대 랜덤 숫자. 최소 랜덤 숫자는 최대 랜덤 숫자의 음수 값</param>
+        /// <returns></returns>
+        private float GetNotOverlappedRandomPosition(List<Vector2> positions, bool findX, float MaxRandomNumber)
+        {
+            float border = 1f;
+            float goodNumber = 0f;
+            bool complete = false;
+            while (!complete)
+            {
+                float randomNumber = Random.Range(-MaxRandomNumber, MaxRandomNumber);
+                IEnumerable<Vector2> result;
+                if (findX)
+                {
+                    result = from position in positions
+                                 where (randomNumber > position.x - border) && (randomNumber < position.x + border)
+                                 select position;
+                }
+                else
+                {
+                    result = from position in positions
+                             where (randomNumber > position.y - border) && (randomNumber < position.y + border)
+                             select position;
+                }
+                if (result == null)
+                {
+                    throw new NullReferenceException();
+                }
+                if (result.Count() == 0)
+                {
+                    goodNumber = randomNumber;
+                    complete = true;
+                }
+            }
+
+            return goodNumber;
+        }
     }
 }
