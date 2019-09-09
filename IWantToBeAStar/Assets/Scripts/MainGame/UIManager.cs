b6bf64f7950aa8ce9420491145fb00e5cc6994c7
Sepi <deet1022@gmail.com>
2019-09-09 18:02:43 +0900

    Revert "Resources 폴더 추가"
    This reverts commit 9a45c99b83db927f1b0d23b09686f98d009fc608.

--- IWantToBeAStar/Assets/Scripts/MainGame/UIManager.cs
+++ IWantToBeAStar/Assets/Scripts/MainGame/UIManager.cs
@@ -68,8 +68,7 @@ namespace IWantToBeAStar.MainGame
         private bool sentInfo = false;
 
         public Text ReadyText { get; private set; }
-        public Text ScoreText { get; private set; }
-        public GameObject ScoreTextObject { get; private set; }
+        public Text ScoreText { get; set; }
 
         private GameOverPanel gameOverPanel;
         private WriteInfoPanel writeInfoPanel;
@@ -84,8 +83,7 @@ namespace IWantToBeAStar.MainGame
             }
 
             ReadyText = GameObject.Find("ReadyText").GetComponent<Text>();
-            ScoreTextObject = Instantiate(Resources.Load("Prefabs/ScoreText") as GameObject, transform, false);
-            ScoreText = ScoreTextObject.GetComponent<Text>();
+            ScoreText = GameObject.Find("ScoreText").GetComponent<Text>();
 
             ReadyText.text = string.Empty;
 
@@ -97,7 +95,6 @@ namespace IWantToBeAStar.MainGame
         private void HandleGameEndedEvent()
         {
             Cursor.visible = true;
-            Destroy(ScoreTextObject);
             Debug.Log("-----[Game Over]-----");
 
             if (gameOverPanel == null)
