540649609510e44c5a87cf363038d67ae2a6a789
Sepi <deet1022@gmail.com>
2019-09-15 00:18:00 +0900

    비행기가 랜덤 스피드로 날아가도록 변경

---
+++ IWantToBeAStar/Assets/Scripts/MainGame/GameStage/InfiniteSpaceStage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IWantToBeAStar.MainGame.GameStage
+{
+    public class InfiniteSpaceStage : Stage
+    {
+        public InfiniteSpaceStage() : base(StageType.Space)
+        {
+        }
+
+        protected override IEnumerator StageMain()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
