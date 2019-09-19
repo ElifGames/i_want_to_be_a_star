8cffac9e968269f687971dc355e04026960a073c
Sepi <deet1022@gmail.com>
2019-09-19 15:08:31 +0900

    update -> fixedUpdate

--- IWantToBeAStar/Assets/Scripts/MainGame/BackgroundScroller.cs
+++ IWantToBeAStar/Assets/Scripts/MainGame/BackgroundScroller.cs
@@ -66,7 +66,7 @@ namespace IWantToBeAStar.MainGame
             bgRotateCount = 0;
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
             if (isGameStarted)
             {
