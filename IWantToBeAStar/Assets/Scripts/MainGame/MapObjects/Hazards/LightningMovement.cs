efcf3db45c5eea72591eb651ef0a9c86160b1904
Sepi <deet1022@gmail.com>
2019-09-22 23:26:42 +0900

    네임스페이스 추가

--- IWantToBeAStar/Assets/Scripts/MainGame/MapObjects/Hazards/LightningMovement.cs
+++ IWantToBeAStar/Assets/Scripts/MainGame/MapObjects/Hazards/LightningMovement.cs
@@ -1,44 +1,48 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class LightningMovement : BaseHazard
+
+namespace IWantToBeAStar.MainGame.MapObjects.Hazards
 {
-    private GameObject lightningWarning;
-    private GameObject lightningHazard;
-
-    protected override void HazardAwake()
+    public class LightningMovement : BaseHazard
     {
-        lightningWarning = gameObject.transform.Find("LightningWarning").gameObject;
-        lightningHazard = gameObject.transform.Find("LightningHazard").gameObject;
-        lightningHazard.SetActive(false);
-        lightningWarning.SetActive(false);
-    }
+        private GameObject lightningWarning;
+        private GameObject lightningHazard;
 
-    protected override void HazardFixedUpdate()
-    {
-    }
-
-    protected override void HazardStart()
-    {
-        StartCoroutine(SpawnLightning());
-    }
-
-    protected override void HazardUpdate()
-    {
-    }
-
-    private IEnumerator SpawnLightning()
-    {
-        for (int i = 0; i < 2; i++)
+        protected override void HazardAwake()
         {
-            lightningWarning.SetActive(true);
-            yield return new WaitForSeconds(0.4f);
+            lightningWarning = gameObject.transform.Find("LightningWarning").gameObject;
+            lightningHazard = gameObject.transform.Find("LightningHazard").gameObject;
+            lightningHazard.SetActive(false);
             lightningWarning.SetActive(false);
-            yield return new WaitForSeconds(0.1f);
         }
-        lightningHazard.SetActive(true);
-        yield return new WaitForSeconds(0.1f);
-        lightningHazard.SetActive(false);
-        Destroy(gameObject);
+
+        protected override void HazardFixedUpdate()
+        {
+        }
+
+        protected override void HazardStart()
+        {
+            StartCoroutine(SpawnLightning());
+        }
+
+        protected override void HazardUpdate()
+        {
+        }
+
+        private IEnumerator SpawnLightning()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                lightningWarning.SetActive(true);
+                yield return new WaitForSeconds(0.4f);
+                lightningWarning.SetActive(false);
+                yield return new WaitForSeconds(0.1f);
+            }
+            lightningHazard.SetActive(true);
+            yield return new WaitForSeconds(0.1f);
+            lightningHazard.SetActive(false);
+            Destroy(gameObject);
+        }
     }
 }
