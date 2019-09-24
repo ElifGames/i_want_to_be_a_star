89220b065b1ce2e6ad7a4aa5358d0394b6fc8dff
Sepi <deet1022@gmail.com>
2019-09-24 20:36:23 +0900

    키보드 이동 관련 수정

--- IWantToBeAStar/Assets/Scripts/MainGame/MapObjects/Player/PlayerMove.cs
+++ IWantToBeAStar/Assets/Scripts/MainGame/MapObjects/Player/PlayerMove.cs
@@ -30,28 +30,6 @@ namespace IWantToBeAStar.MainGame.MapObjects.Player
                     MouseControl();
                     break;
             }
-        }
-
-        private void KeyboardControl()
-        {
-            float inputX = Input.GetAxis("Horizontal");
-            float inputY = Input.GetAxis("Vertical");
-            Vector2 velocity = new Vector2(inputX, inputY);
-            velocity *= KeyboardSpeed;
-            rigidbody2d.velocity = velocity;
-            rigidbody2d.position = new Vector2(
-                Mathf.Clamp(rigidbody2d.position.x, MapSize.xMin, MapSize.xMax),
-                Mathf.Clamp(rigidbody2d.position.y, MapSize.yMin, MapSize.yMax));
-        }
-
-        private void MouseControl()
-        {
-            // 그냥 마우스 따라 움직이는 코드
-            //transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            //부드럽게 움직이는 코드
-            transform.position =
-                Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), CursorSpeed * Time.deltaTime);
 
             // 플레이어가 맵 밖을 나가지 않도록 하는 코드
             transform.position = new Vector2(
@@ -59,6 +37,22 @@ namespace IWantToBeAStar.MainGame.MapObjects.Player
                 Mathf.Clamp(transform.position.y, MapSize.yMin, MapSize.yMax));
         }
 
+        private void KeyboardControl()
+        {
+            float inputX = Input.GetAxis("Horizontal");
+            float inputY = Input.GetAxis("Vertical");
+            
+            float moveX = inputX * KeyboardSpeed * Time.deltaTime;
+            float moveY = inputY * KeyboardSpeed * Time.deltaTime;
+            transform.Translate(new Vector2(moveX, moveY));
+        }
 
+        private void MouseControl()
+        {
+            var move = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            transform.position =
+                Vector2.Lerp(transform.position, move, CursorSpeed * Time.deltaTime);
+        }
     }
 }
